3c67a541c43d1d358ecd6da8412631d096dc5d52
TSHEPO DAVID MOLEFE <tshepomolefeda@gmail.com>
2023-06-15 13:01:27 +0200

    Add images

--- my-app/pages/api/[tokenId].js
+++ my-app/pages/api/[tokenId].js
@@ -2,11 +2,13 @@ export default function handler(req, res) {
  
     const tokenId = req.query.tokenId;
   
-    const src = "encode-capstone-project/my-app/public/encodedevs/";
+    
+    const image_url =  "https://raw.githubusercontent.com/TSHEPO-CLOUD/encode-capstone-project/main/my-app/public/encodedevs/0.svg";
+    
     
     res.status(200).json({
       name: "Encode Dev #" + tokenId,
       description: "Encode Dev is a collection of Encode Club developers in crypto",
-      image: src + tokenId + ".svg",
+      image: image_url + tokenId + ".svg",
     });
   }
