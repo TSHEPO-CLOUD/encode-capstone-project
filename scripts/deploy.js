f1df84ba1402cfee7832b694f3e46a8255c326e4
TSHEPO DAVID MOLEFE <tshepomolefeda@gmail.com>
2023-06-15 12:10:48 +0200

    Modifie css

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,9 +3,9 @@ require("dotenv").config({ path: ".env" });
 const { WHITELIST_CONTRACT_ADDRESS, METADATA_URL } = require("../constants");
 
 async function main() {
-  // Address of the whitelist contract that you deployed in the previous module
+
   const whitelistContract = WHITELIST_CONTRACT_ADDRESS;
-  // URL from where we can extract the metadata for a Crypto Dev NFT
+ 
   const metadataURL = METADATA_URL;
   
  const encodeDevsContract = await hre.ethers.deployContract("EncodeDevs", [
