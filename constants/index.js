c8cbec12aa138399236bf1fa43036dbff18e60a4
TSHEPO DAVID MOLEFE <tshepomolefeda@gmail.com>
2023-06-15 12:36:34 +0200

    Edit my-app index.js witha new contract address

--- constants/index.js
+++ constants/index.js
@@ -1,5 +1,5 @@
 const WHITELIST_CONTRACT_ADDRESS = "0xf26781cd5e17288b779B3275AA3A09688c58bb4e";
 
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://encode-capstone-project.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
